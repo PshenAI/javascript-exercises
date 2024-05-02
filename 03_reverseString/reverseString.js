ce9ce6d0e5e96f85685edcaaa0efedb917d1a3d3
pshenai <antonvoloshyn25@gmail.com>
2024-05-02 19:35:16 +0300

    Add solutions to exercises 3, 4, 5, 6 and 7.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(toReverse) {
+    const splittedString = toReverse.split("");
 
+    let res = "";
+    for(let i = splittedString.length - 1; i >= 0; i--) {
+        res += splittedString[i];
+    }
+    return res;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(initNum, lastNum) {
+    if((initNum < 0 || lastNum < 0) ||
+        (typeof initNum !== "number" || typeof lastNum !== "number")) {
+        return "ERROR";
+    }
 
+    if(initNum > lastNum) {
+        let buffer = lastNum;
+        lastNum = initNum;
+        initNum = buffer;
+    }
+
+    let sum = initNum;
+
+    for (let i = initNum + 1; i <= lastNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
