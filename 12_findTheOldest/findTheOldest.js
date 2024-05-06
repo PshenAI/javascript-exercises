260b4cd6ace24afeaaff7a3447304a489827daea
pshenai <antonvoloshyn25@gmail.com>
2024-05-06 18:32:38 +0300

    Add solutions to exercise 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldestPersonAge = 0;
 
+    return arr.reduce((oldestPerson, person) => {
+        let personAge;
+        if(person.yearOfDeath === undefined) {
+            personAge = (new Date).getFullYear() - person.yearOfBirth
+        } else {
+            personAge = person.yearOfDeath - person.yearOfBirth;
+        }
+
+
+        if(oldestPerson === undefined || personAge > oldestPersonAge) {
+            oldestPersonAge = personAge;
+            return person;
+        } else {
+            return oldestPerson;
+        }
+    }, {});
 };
 
 // Do not edit below this line
