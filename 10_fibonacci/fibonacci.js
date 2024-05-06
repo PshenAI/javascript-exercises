3b8b86c3c50767980f1cc67246817018fb9a267f
pshenai <antonvoloshyn25@gmail.com>
2024-05-06 17:05:29 +0300

    Add solutions to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    num = Number(arg);
 
+    if(num < 0) {
+        return 'OOPS';
+    } else if(num === 1 || num === 2) {
+        return 1;
+    } else if(num === 0) {
+        return 0;
+    }
+
+    let fibonacciNum = 1;
+    let previousNum = 1;
+
+    for (let i = 0; i < num - 2; i++) {
+        let temp = fibonacciNum;
+        fibonacciNum += previousNum;
+        previousNum = temp;
+    }
+
+    return fibonacciNum;
 };
 
 // Do not edit below this line
