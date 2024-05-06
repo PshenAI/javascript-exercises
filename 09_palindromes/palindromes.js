33d102a49091fde9d03d1dbf09ab127e914b4e85
pshenai <antonvoloshyn25@gmail.com>
2024-05-06 16:32:55 +0300

    Add solutions to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const punctuation = /[\.,?!\s]/g;
+    let newText = str.toLowerCase().replace(punctuation, "");
 
+    const reversedStr = newText.split("").reverse().join("");
+
+    return newText === reversedStr;
 };
 
 // Do not edit below this line
